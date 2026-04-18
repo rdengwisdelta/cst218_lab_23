5008a6d6aa299a4fa3e97f1c9f5c69f56a370958
rdengwisdelta <ryanengwis@delta.edu>
2026-04-18 11:01:41 -0400

    edited to initialize dotenv package first

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+// Load environment variables from .env
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
-// Load environment variables from .env
-require("dotenv").config();
 
 const User = require("./models/User");
 const Entry = require("./models/Entry");
